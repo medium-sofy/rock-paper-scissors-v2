64d0d3e5704a79b79af28752b19584950a0fda3f
Mohamed Sofy <mediumsofy@gmail.com>
2024-07-13 14:36:12 +0300

    Refactor: Add buttons to the game, and modify game mechanics
    This commit adds buttons for the user to interact with, instead of the browser alert prompt,
    as well as make the game end when either player gets a score of 5, instead of playing exactly five rounds

--- script.js
+++ script.js
@@ -6,21 +6,69 @@
     -You will likely have to refactor (rework/rewrite) your original code to make it work for this. That’s OK! Reworking old code is an important part of a programmer’s life.
  */
 
-
-function getRandomInt(int) {
-  return Math.floor(Math.random() * int) + 1;
-}
-
-function getComputerChoice() {
-  choice = getRandomInt(3);
-  if (choice === 1) return "rock";
-  if (choice === 2) return "paper";
-  if (choice === 3) return "scissors";
-}
-
 // global variables to be used in playRound and playGame
+
 let playerScore = 0;
 let computerScore = 0;
+
+const rock = document.querySelector("#rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+const message = document.querySelector(".round-message");
+const scores = document.querySelector(".scores");
+
+rock.addEventListener("click", () => {
+  if (playerScore < 5 && computerScore < 5) {
+  
+    // playRound determines the winner of the round, increments his score by one,
+    // and returns a string announcing the winner of the round
+    message.textContent = playRound("rock", getComputerChoice());
+  
+    if (playerScore === 5) {
+      message.textContent = `Game ended! you won the game!`;
+    }
+    if (computerScore === 5) {
+      message.textContent = `Game ended! you lost the game!`;
+    }
+  }
+  scores.textContent = `your score is ${playerScore}, and the computer score is ${computerScore}`;
+  console.log(message.textContent);
+});
+
+
+paper.addEventListener("click", () => {
+  if (playerScore < 5 && computerScore < 5) {
+   
+    message.textContent = playRound("paper", getComputerChoice());
+   
+    if (playerScore === 5) {
+      message.textContent = `Game ended! you won the game!`;
+    }
+    if (computerScore === 5) {
+      message.textContent = `Game ended! you lost the game!`;
+    }
+  }
+  scores.textContent = `your score is ${playerScore}, and the computer score is ${computerScore}`;
+  console.log(message.textContent);
+});
+
+
+scissors.addEventListener("click", () => {
+  if (playerScore < 5 && computerScore < 5) {
+    
+    message.textContent = playRound("scissors", getComputerChoice());
+    
+    if (playerScore === 5) {
+      message.textContent = `Game ended! you won the game!`;
+    }
+    if (computerScore === 5) {
+      message.textContent = `Game ended! you lost the game!`;
+    }
+  }
+  scores.textContent = `your score is ${playerScore}, and the computer score is ${computerScore}`;
+  console.log(message.textContent);
+});
+
 function playRound(player, computer) {
   // convert player input to lowercase to make it case insensitive
   player = player.toLowerCase();
@@ -31,37 +79,37 @@ function playRound(player, computer) {
   }
 
   if (player == "rock" && computer == "paper") {
-    computerScore++;
+    ++computerScore;
     return "You lose! paper beats rock";
   }
 
   if (player == "rock" && computer == "scissors") {
-    playerScore++;
+    ++playerScore;
     return "You Win! Rock beats scissors";
   }
 
   // Player plays paper
 
   if (player == "paper" && computer == "rock") {
-    playerScore++;
+    ++playerScore;
     return "You Win! Paper beats rock";
   }
   if (player == "paper" && computer == "paper") {
     return "It's a Tie!";
   }
   if (player == "paper" && computer == "scissors") {
-    computerScore++;
+    ++computerScore;
     return "You lose! Scissors beats paper";
   }
 
   // Player plays scissors
 
   if (player == "scissors" && computer == "rock") {
-    computerScore++;
+    ++computerScore;
     return "You lose! Rock beats scissors";
   }
   if (player == "scissors" && computer == "paper") {
-    playerScore++;
+    ++playerScore;
     return "You Win! scissors beats paper";
   }
   if (player == "scissors" && computer == "scissors") {
@@ -69,27 +117,14 @@ function playRound(player, computer) {
   }
 }
 
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-    playerSelection = prompt("Rock, Paper, or Scissors?");
-    console.log(playRound(playerSelection, getComputerChoice()));
-    console.log(computerScore, playerScore);
-  }
+// helper methods
+function getRandomInt(int) {
+  return Math.floor(Math.random() * int) + 1;
 }
-// Main game loop
-playGame();
 
-// Check the winner
-if (computerScore > playerScore) {
-  console.log(
-    `You lost the game.. your score is ${playerScore}, and the computer score is ${computerScore}`
-  );
-} else if (computerScore < playerScore) {
-  console.log(
-    `You Won the game! your score is ${playerScore}, and the computer score is ${computerScore}`
-  );
-} else {
-  console.log(
-    `Game over, It's a tie, your score is ${playerScore}, and the computer score is ${computerScore}`
-  );
+function getComputerChoice() {
+  choice = getRandomInt(3);
+  if (choice === 1) return "rock";
+  if (choice === 2) return "paper";
+  if (choice === 3) return "scissors";
 }
